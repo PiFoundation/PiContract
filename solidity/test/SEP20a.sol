6972e565139d4a04afa7dbecc55efd1678015289
MrHonekawa <76059318+MrHonekawa@users.noreply.github.com>
2022-01-20 23:57:05 -0800

    Update SEP20a.sol

--- solidity/test/SEP20a.sol
+++ solidity/test/SEP20a.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC20 {
+contract PiToken is ERC20 {
     constructor(string memory name, string memory symbol) ERC20("Pi", "π") {
         // Mint 100 pi to msg.sender
         // Similar to
